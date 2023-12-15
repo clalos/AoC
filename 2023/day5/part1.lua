a3bb6c9f0c46ac183a66d248eddce1b66feadd04
Claudio Loseto <loseto@pm.me>
2023-12-15 18:24:13 +0100

    add day 5 part 1 2023

---
+++ 2023/day5/part1.lua
@@ -0,0 +1,40 @@
+local isMap, seeds, map = false, {}, {}
+
+-- moveSeeds moves seeds according to the map
+local function moveSeeds(seeds, map)
+    local seedsMoved = {}
+    for _, instr in ipairs(map) do
+        for k, seed in ipairs(seeds) do
+            if not seedsMoved[k] and instr.source + instr.length > seed and instr.source <= seed then
+                seeds[k] = instr.dest + (seed - instr.source)
+                seedsMoved[k] = true
+            end
+        end
+    end
+    return seedsMoved
+end
+
+-- read input
+for line in io.lines("input.txt") do
+    if line:find("seeds") then
+        for s in line:gmatch("%d+") do
+            table.insert(seeds, tonumber(s))
+        end
+    end
+    if isMap and line ~= "" then
+        local dest, source, length = line:match("(%d+) (%d+) (%d+)")
+        table.insert(map, { dest = tonumber(dest), source = tonumber(source), length = tonumber(length) })
+    end
+    if line:find("map") then
+        isMap = true
+    end
+    if line == "" then
+        moveSeeds(seeds, map)
+        map = {}
+        isMap = false
+    end
+end
+
+moveSeeds(seeds, map)
+table.sort(seeds)
+print(seeds[1])
