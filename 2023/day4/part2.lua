2df86951676b29fd1ca2907c115bbfbfd68746a4
Claudio Loseto <loseto@pm.me>
2023-12-15 13:10:04 +0100

    add day4 part 2 2023

---
+++ 2023/day4/part2.lua
@@ -0,0 +1,47 @@
+-- load games
+local cards = {}
+for line in io.lines("input.txt") do
+    local cardNo, winningNo, haveNo = line:match("(%d+): (.+)|(.+)")
+
+    local winningList = {}
+    for num in winningNo:gmatch("%d+") do
+        winningList[num] = true
+    end
+
+    local haveList = {}
+    for num in haveNo:gmatch("%d+") do
+        haveList[num] = true
+    end
+
+    cards[tonumber(cardNo)] = { winning = winningList, have = haveList, count = 1 }
+end
+
+-- getWins returns the number of wins a card has.
+local function getWins(card)
+    local wins = 0
+    for winningNo, _ in pairs(card.winning) do
+        if card.have[winningNo] then
+            wins = wins + 1
+        end
+    end
+    return wins
+end
+
+-- play returns the number of cards that can be won by playing this card.
+local function play(cardNo, card)
+    local wins = getWins(card)
+    local count = 1
+    for i = 1, wins do
+        local nextCardNo = cardNo + i
+        if not cards[nextCardNo] then return 0 end
+        count = count + play(nextCardNo, cards[nextCardNo])
+    end
+    return count
+end
+
+-- play all cards
+local totalCards = 0
+for cardNo, card in pairs(cards) do
+    totalCards = totalCards + play(cardNo, card)
+end
+print(totalCards)
